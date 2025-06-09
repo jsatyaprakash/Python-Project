ae40b8b46cb3b71199455d796ae7e10256e1050c
jsatyaprakash <jamadars607@gmail.com>
2025-06-09 21:43:44 +0530

    commit

---
+++ python one.py
@@ -0,0 +1,231 @@
+"""print("satyaprakash jamadar")
+print("satyaprakash jamadars")
+print("age 20")
+print("my name is satyaprakash jamadar.","my age is 20.")
+print("23")
+print("35")
+print("23")
+print("35+23")
+name="satyaprakash jamadar"
+age=20
+price=25.99
+
+print(name)
+print(age)
+print(price)
+print("my name is:",name)
+print("my age is:",age)
+
+a = 2
+b = 5
+sum = a + b
+print(sum)
+a = 1000
+b = 500
+sum = a + b
+print(sum)
+a = 1000
+b = a - b
+print(b)
+
+print ("hello world")
+print("hello world")
+
+print("hello world")
+#print("hello world")
+
+print("hello world")
+"""
+# multi-line
+# comment
+"""
+print("hello word")
+print("hello word")
+print("hello word")
+print("hello word")
+
+print("hello word")
+#print("hello word")
+#print("hello word")
+#print("hello word")
+
+a = 5
+b = 2
+
+sum = a + b
+print(sum)
+
+a = 5
+b = 2
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b) #remainder
+print(a ** b) #a^b
+
+#relational operators
+a = 50
+b = 20
+
+print(a == b) #False
+print(a != b) #True
+print(a >= b) #True
+print(a > b) #true
+print(a >= b) #False
+print(a < b) #False
+
+#assignment operators
+num = 10
+num = num + 10 #10+10 => 20
+print(num)
+
+num = 10
+num = num + 10
+num += 10
+print("num :", num)
+
+num = 10
+num -= 10
+print("num :, num") #0
+
+num = 10
+num *= 5
+print("num :", num) #50
+
+num = 10
+num /= 5
+print("num :", num) #2
+
+num = 10
+num **= 5
+print("num :", num) #1,00,000
+
+#logical operators
+print (not False)
+print(not True)
+
+a = 50
+b = 30
+print(not False)
+print(not (a > b))
+
+val1 = True
+val2 = True
+print("ans operator:", val1 and val2 )
+
+a = 50
+b = 30
+print(not False)
+print(not (a > b))
+
+val1 = True
+val2 = False
+print("and operator:", val1 and val2 )
+
+a = 50
+b = 30
+print(not False)
+print(not (a > b))
+
+val1 = False
+val2 = False
+print("AND operator:", val1 and val2 )
+
+print("OR operator:", val1 or val2)
+
+a= 50
+b = 30
+print(not False)
+print(not (a > b))
+
+val1 = False
+val2 = False
+print("AND operator:", val1 and val2 )
+
+print("OR operator:",(a == b) or (a > b))
+
+#type conversion
+a = 2
+b = 4.25
+
+sum = a + b # 2.0 + 4.25 => 6.25
+print(sum)
+
+a = "2"
+b = 4.25
+
+print(type(a))
+
+a = int("2")
+b = 4.25
+
+print(type(a))
+print(type(a + b))
+
+a = float("2")
+b = 4.25
+
+print(type(a))
+print(type(a + b))
+
+a = 3.14
+a = str(a)
+
+print(type(a))
+
+name = input("enter your age:")
+print("your entered", name)
+
+val = input("enter some value:")
+print(type (val), val) #25 , "99.99"
+
+int("5")
+val = int(input ("enter some value:"))
+print(type(val), val)
+
+int("5")
+val = float(input ("enter some value: "))
+print(type(val, val))
+
+name = input("enter name: ")
+age = input("enter age: ")
+marks = input("enter marks")
+
+print("welcome", name)
+print("age=", age)
+print("marks =", marks)
+
+name = input("enter name: ")
+age =  int(input("enter age: "))
+marks =float(input("enter marks"))
+
+print("welcome", name)
+print("age=", age)
+print("marks =", marks)
+
+first = int(input("enter first: "))
+second =(input("enter second: "))
+
+print("sum =", first + second)
+
+side = float(input("enter square : "))
+print("area =", side * side)
+
+a = float(input("enter first : "))
+b = float(input("enter second : "))
+
+print("avg=", (a+b)/2)
+
+a =int(input("enter fist : "))
+b =int(input("enter secode"))
+
+print(a >= b)"""
+
+num = int(input("enter a number: "))
+
+if num % 2 == 0 :
+    print(f"{num} is Even")
+else :
+    print(f"{num} is odd")
---
+++ python two.py
@@ -0,0 +1,70 @@
+str1 = "this is a string."
+str2 = 'apnacollege'
+str3 = """this is a string"""
+
+'this is apnacolleg"s tutorial'
+
+"""str1 = "this is a string.\n we are creating it in python"
+print(str1)
+
+str1 = "apna"
+str2 = "college"
+print(str1 + str2)
+
+str1 = "apna"
+str2 = "college"
+final_str = str1+str2
+print(final_str)"""
+
+str1 = "apna"
+print(len(str1))
+str2 = "college"
+final_str = str1 + str2
+print(final_str)
+
+str1 = "apna"
+len1 = len(str1)
+print(len1)
+
+str2 = "colleg"
+len2 = len(str2)
+print(len2)
+
+final_str = str1 + " " + str2
+print(final_str)
+
+str1 = "apna college"
+ch = str1[1]
+print(ch)
+
+str = "apna college"
+ch = str1[2]
+print(ch)
+
+str = "apna college"
+ch = str1[3]
+print(ch)
+
+str = "apna college"
+print(str[4])
+
+str =" apna college"
+print(str[1:4])
+str = "apna college"
+print(str[1:5])
+str ="apna college"
+print(str[0:4])
+str = "apna college"
+print(str[5:12])
+str = "apna college"
+print(str[5:len(str)])
+str = "apna colllege"
+print (str[:4]) #[0:4]
+print(str[:]) #[5::ien(str)]
+
+str = "apple"
+print(str[-3:-1])
+str = "apple"
+print(str[-5:-2])
+str  = "apple"
+print(str[ -5:-1])
